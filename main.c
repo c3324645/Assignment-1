71ffc23d62703559dc4bbb5c9f60788e70518be6
Stephanie McManus <stephy.mcmanus@gmail.com>
2019-04-02 23:59:58 +0000

    added more comments, tried to initialise variables

--- main.c
+++ main.c
@@ -6,11 +6,18 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // define function for the rotation cipher
 
 // ask the user for the plain text to encrypt
 int main() {
+    char message[100], ch;
+    int key, i;
+    /* printf("enter a message to encrypt: ")
+    get(message)
+    printf("enter key: ") */
+    
 
   return 0;
 }
