d1c96b798a0297473c767a1574fde7ba7375efd3
Stephanie McManus <stephy.mcmanus@gmail.com>
2019-04-02 23:21:36 +0000

    added some comments about the rotation cipher, added #include <string.h>

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
+/*
+ Title: Rotation Cipher
+ Description: this program takes in a plain text and produces a cipher of that text using the rotation cipher
+ 
+*/
 
+#include <stdio.h>
+#include <string.h>
+
+// define function for the rotation cipher
+
+// ask the user for the plain text to encrypt
 int main() {
 
   return 0;
