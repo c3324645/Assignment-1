fdf78b02eff32db0591f0962ee00c39e09b4731b
Stephanie McManus <stephy.mcmanus@gmail.com>
2019-04-10 01:08:57 +0000

    made a new folder and c-file for the rotation ciphers for organisation purposes. Added some comments. Tried a different way of making a rotation cipher

---
+++ rotation/encryption.c
@@ -0,0 +1,15 @@
+/*
+    A rotation/caesar cipher which takes input and encrypts it by shifting the letters
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //declare variables
+    int key
+    char 
+    printf("hello, world\n");
+}
